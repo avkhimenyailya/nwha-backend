9924359ed10d7d0289c826170d9e436704f77e57
Ilya Avkhimenya <tmnkilya@gmail.com>
2023-04-03 16:45:36 +0300

    Сообщение об ошибке в формате JSON

--- src/main/java/io/grayproject/nwha/api/advice/ExceptionHandlerAdvice.java
+++ src/main/java/io/grayproject/nwha/api/advice/ExceptionHandlerAdvice.java
@@ -1,5 +1,8 @@
 package io.grayproject.nwha.api.advice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,9 +11,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<String> handleConflict(Exception exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    public ResponseEntity<String> handleConflict(Exception exception) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("message", exception.getMessage());
+        return ResponseEntity.badRequest().body(objectMapper.writeValueAsString(objectNode));
     }
-
-
 }

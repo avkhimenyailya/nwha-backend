e7ad3110bb7de5e7590561346344e988c9e1e01f
Ilya Avkhimenya <tmnkilya@gmail.com>
2023-04-08 19:35:54 +0300

    Ссылка img открыта для всех

--- src/main/java/io/grayproject/nwha/api/security/SecurityConfig.java
+++ src/main/java/io/grayproject/nwha/api/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        final String[] permittedEndpoints = {"/", "/css/**", "/error", "/auth/**"};
+        final String[] permittedEndpoints = {"/", "/css/**", "/error", "/auth/**", "/img"};
 
         return http
                 .cors().and()

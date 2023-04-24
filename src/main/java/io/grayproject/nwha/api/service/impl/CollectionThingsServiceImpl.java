9f53c8a1d2a3fba43aabff96fc81bae1d7b8f9f6
Ilya Avkhimenya <tmnkilya@gmail.com>
2023-04-24 13:07:18 +0300

    Фильтрация CollectionThings по удаленным

--- src/main/java/io/grayproject/nwha/api/service/impl/CollectionThingsServiceImpl.java
+++ src/main/java/io/grayproject/nwha/api/service/impl/CollectionThingsServiceImpl.java
@@ -149,6 +149,7 @@ public class CollectionThingsServiceImpl implements CollectionThingsService {
         return collectionThingsRepository
                 .findAllByProfileId(profileId)
                 .stream()
+                .filter(collectionThings -> !collectionThings.getRemoved())
                 .map(collectionThingsMapper)
                 .toList();
     }

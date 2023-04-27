b249ae46a0e26b9fcfbb758fc3f86226c3889508
Ilya Avkhimenya <tmnkilya@gmail.com>
2023-04-27 14:55:41 +0300

    Добавлен produces MediaType.ALL_VALUE при получении изображения

--- src/main/java/io/grayproject/nwha/api/controller/ImgController.java
+++ src/main/java/io/grayproject/nwha/api/controller/ImgController.java
@@ -1,5 +1,6 @@
 package io.grayproject.nwha.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,22 +12,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 
 /**
  * @author Ilya Avkhimenya
  */
+@Slf4j
 @RestController
 @RequestMapping("/img")
 public class ImgController {
 
-    @GetMapping("/{name}")
+    @GetMapping(name = "/{name}", produces = {
+//            MediaType.IMAGE_GIF_VALUE,
+//            MediaType.IMAGE_PNG_VALUE,
+//            MediaType.IMAGE_JPEG_VALUE,
+            MediaType.ALL_VALUE
+    })
     public ResponseEntity<InputStreamResource> getFileFromFtpServerByPath(
-            @PathVariable String name) throws IOException {
+            @PathVariable String name) throws FileNotFoundException {
         File file = new File("images/" + name);
-        String mimeType = Files.probeContentType(file.toPath());
+
+        String mimeType;
+        try {
+            mimeType = Files.probeContentType(file.toPath());
+        } catch (IOException e) {
+            log.debug("Ошибка произошла при получении mimeType");
+            throw new RuntimeException(e);
+        }
+
         InputStreamResource inputStreamResource = new InputStreamResource(new FileInputStream(file));
+
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.asMediaType(MimeType.valueOf(mimeType)))
